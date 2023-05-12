88eecf622e91e2606f9ecf0f6f4a04ace95f18a8
daniel021-hub <daes021@hotmail.com>
2023-05-11 23:32:53 -0400

    final commit

--- src/utils/database.js
+++
@@ -1,9 +0,0 @@
-const {Sequelize} = require('sequelize')
-
-const config = require('../../config')
-
-const environment = config.nodeEnv
-
-const db = new Sequelize(config.db[environment])
-
-module.exports = db
